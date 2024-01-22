09dd06a4752fe19e4a0cb0f74b606ed7e655d116
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-01-22 14:28:01 +0100

    leaflet gpx viewer (WIP)

--- src/rando_planner/diagram.clj
+++ src/rando_planner/diagram.clj
@@ -4,6 +4,12 @@
             [rando-planner.gpx :as gpx]
             [rando-planner.plan :as plan]))
 
+(alter-var-root #'nextjournal.clerk.view/include-css+js
+                (fn [include-fn]
+                  (fn [state]
+                    (concat (include-fn state)
+                            (list (hiccup.page/include-css "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"))))))
+
 (defn viewbox-dimensions-to-str [viewbox-dimensions]
   (str/join " " (map str viewbox-dimensions)))
 
@@ -236,3 +242,37 @@
                                              average-speed
                                              elevation)]))
          output))]))
+
+
+(def leaflet-gpx-viewer
+  {:transform-fn clerk/mark-presented
+   :render-fn '(fn [value]
+                 (when value
+                   [nextjournal.clerk.render/with-d3-require
+                    {:package ["leaflet@1.7.1/dist/leaflet.min.js"
+                               "leaflet-gpx@1.5.1/gpx.min.js"
+                               ]}
+                    (fn [leaflet]
+                      [:div {:id "lmap"
+                             :height "400px"
+                             :style {:height "400px"}
+                             :ref (fn [el]
+                                    (when el
+                                      (let [attribution "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+                                            marker-options {:startIconUrl "https://stefanorodighiero.net/misc/pin-icon-start.png"
+                                                            :endIconUrl "https://stefanorodighiero.net/misc/pin-icon-end.png"
+                                                            :shadowUrl "https://stefanorodighiero.net/misc/pin-shadow.png"}
+                                            m (.map js/L "lmap")
+                                            tile-layer (.tileLayer js/L "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                                                   (clj->js {:attribution attribution}))
+                                            gpx-layer (new js/L.GPX (:gpx value)
+                                                           (clj->js {:async true
+                                                                     :marker_options marker-options}))]
+                                        (.setView m
+                                                  (clj->js (:center value))
+                                                  (:zoom value))
+                                        (.addTo tile-layer m)
+                                        (.addTo gpx-layer m)
+                                        )))}])]))})
+
+
