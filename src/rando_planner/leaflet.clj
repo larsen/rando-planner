7878e2fc5ef993ace6f5014f767050190ece7bb2
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-01-28 16:04:36 +0100

    Allow multiple maps in the same notebook

--- src/rando_planner/leaflet.clj
+++ src/rando_planner/leaflet.clj
@@ -9,25 +9,26 @@
                     {:package ["leaflet@1.7.1/dist/leaflet.min.js"
                                "leaflet-gpx@1.5.1/gpx.min.js"]}
                     (fn [leaflet]
-                      [:div {:id "lmap"
-                             :height "400px"
-                             :style {:height "400px"}
-                             :ref (fn [el]
-                                    (when el
-                                      (let [attribution "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
-                                            marker-options {:startIconUrl "https://stefanorodighiero.net/misc/pin-icon-start.png"
-                                                            :endIconUrl "https://stefanorodighiero.net/misc/pin-icon-end.png"
-                                                            :shadowUrl "https://stefanorodighiero.net/misc/pin-shadow.png"}
-                                            m (.map js/L "lmap")
-                                            tile-layer (.tileLayer js/L "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-                                                                   (clj->js {:attribution attribution}))
-                                            gpx-layer (new js/L.GPX (or (:gpx value)
-                                                                        (:gpx-content value))
-                                                           (clj->js {:async true
-                                                                     :marker_options marker-options}))]
-                                        (.setView m
-                                                  (clj->js (:center value))
-                                                  (:zoom value))
-                                        (.addTo tile-layer m)
-                                        (.addTo gpx-layer m)
-                                        )))}])]))})
+                      (let [map-div-id (str (gensym))]
+                        [:div {:id map-div-id
+                               :height "400px"
+                               :style {:height "400px"}
+                               :ref (fn [el]
+                                      (when el
+                                        (let [attribution "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+                                              marker-options {:startIconUrl "https://stefanorodighiero.net/misc/pin-icon-start.png"
+                                                              :endIconUrl "https://stefanorodighiero.net/misc/pin-icon-end.png"
+                                                              :shadowUrl "https://stefanorodighiero.net/misc/pin-shadow.png"}
+                                              m (.map js/L map-div-id)
+                                              tile-layer (.tileLayer js/L "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                                                     (clj->js {:attribution attribution}))
+                                              gpx-layer (new js/L.GPX (or (:gpx value)
+                                                                          (:gpx-content value))
+                                                             (clj->js {:async true
+                                                                       :marker_options marker-options}))]
+                                          (.setView m
+                                                    (clj->js (:center value))
+                                                    (:zoom value))
+                                          (.addTo tile-layer m)
+                                          (.addTo gpx-layer m)
+                                          )))}]))]))})
