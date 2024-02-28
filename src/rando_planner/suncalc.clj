067a4f57bf23a41fbe4af0734fd3b89d96b25f33
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-02-28 16:59:27 +0100

    WIP

--- src/rando_planner/suncalc.clj
+++ src/rando_planner/suncalc.clj
@@ -53,6 +53,32 @@
      (* 0.0053 (Math/sin mean-anomaly-rad))
      (- (* 0.0069 (Math/sin (* 2 ecliptic-longitude-rad))))))
 
+(defn hour-angle [lat elevation L-rad]
+  ;; (
+  ;;   sin(
+  ;;     radians(-0.833 - 2.076 * sqrt(elevation) / 60.0)
+  ;;   )
+  ;;   - sin(radians(f)) * sin_d
+  ;; )
+  ;; / (cos(radians(f)) * cos_d)
+  (let [ ;; Declination of the sun
+        obliquity 23.4397
+        sin-d (* (Math/sin L-rad)
+                 (Math/sin (Math/toRadians obliquity)))
+        cos-d (Math/cos (Math/asin sin-d))
+
+        lat-rad (Math/toRadians lat)
+        w0-cos (/ (- (Math/sin
+                      (Math/toRadians
+                       (- -0.833
+                          (/ (* 2.076 (Math/sqrt elevation))
+                             60.0))))
+                     (* (Math/sin lat-rad)
+                        sin-d))
+                  (* (Math/cos lat-rad)
+                     cos-d))]
+    (Math/toDegrees (Math/acos w0-cos))))
+
 (defn sunset-sunrise-times [ts lat lon elevation]
   (let [j (ts->julian-date ts)
         ;; Julian day (from Jan 1st, 2000)
@@ -75,32 +101,8 @@
                                  M-rad
                                  L-rad)
 
-        ;; Declination of the Sun
-        obliquity 23.4397
-        sin-d (* (Math/sin L-rad)
-                 (Math/sin (Math/toRadians obliquity)))
-        cos-d (Math/cos (Math/asin sin-d))
-
         ;; Hour angle
-
-        ;; (
-        ;;   sin(
-        ;;     radians(-0.833 - 2.076 * sqrt(elevation) / 60.0)
-        ;;   )
-        ;;   - sin(radians(f)) * sin_d
-        ;; )
-        ;; / (cos(radians(f)) * cos_d)
-        lat-rad (Math/toRadians lat)
-        w0-cos (/ (- (Math/sin
-                      (Math/toRadians
-                       (- 0.833
-                          (/ (* 2.076 (Math/sqrt elevation))
-                             60.0))))
-                     (* (Math/sin lat-rad)
-                        sin-d))
-                  (* (Math/cos lat-rad)
-                     cos-d))
-        w0-deg (Math/toDegrees (Math/acos w0-cos))
+        w0-deg (hour-angle lat elevation L-rad)
 
         j-rise (- J-transit (/ w0-deg 360))
         j-set (+ J-transit (/ w0-deg 360))]
--- test/rando_planner/suncalc_test.clj
+++ test/rando_planner/suncalc_test.clj
@@ -9,6 +9,10 @@
    (< (Math/abs (- a b))
       epsilon)))
 
+(def lat 33.00801)
+(def lon 35.08794)
+(def elevation 0)
+
 (deftest suncalc-tests
   (testing "Julian date & day"
     (is (epsilon-diff (sut/ts->julian-date 1709128197.3056235)
@@ -16,7 +20,7 @@
     (is (= (sut/julian-date->julian-day 2460369.061)
            8825.0)))
   (testing "Mean solar time"
-    (is (= (sut/mean-solar-time 8825.0 35.08794)
+    (is (= (sut/mean-solar-time 8825.0 lon)
            8824.903433500)))
   (testing "Solar mean anomaly"
     (is (epsilon-diff (sut/mean-anomaly 8824.903433500)
@@ -36,5 +40,14 @@
                                           0.966
                                           5.932))
                       1709200418.3787212
-                      0.2 ; Double check! Epsilon too large
-                      ))))
+                      0.2            ; Double check! Epsilon too large
+                      )))
+  (testing "Hour angle"
+    (is (epsilon-diff (sut/hour-angle
+                       lat
+                       elevation
+                       (sut/ecliptic-longitude
+                        (sut/mean-anomaly 8824.903433500)
+                        (sut/equation-of-the-center
+                         (Math/toRadians 55.356))))
+                      85.860))))
