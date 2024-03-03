8b8915310696e5b69b8426ac8db238289bb6dad7
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-03-03 13:59:31 +0100

    WIP

--- test/rando_planner/suncalc_test.clj
+++ test/rando_planner/suncalc_test.clj
@@ -1,10 +1,11 @@
 (ns rando-planner.suncalc-test
   (:require [rando-planner.suncalc :as sut]
+            [clj-time.core :as cjt]
             [clojure.test :refer [deftest is testing]]))
 
-(defn epsilon-diff
+(defn small-diff
   ([a b]
-   (epsilon-diff a b 0.001))
+   (small-diff a b 0.001))
   ([a b epsilon]
    (< (Math/abs (- a b))
       epsilon)))
@@ -12,42 +13,47 @@
 (def lat 33.00801)
 (def lon 35.08794)
 (def elevation 0)
+(def timestamp 1709128197.3056235)
 
 (deftest suncalc-tests
   (testing "Julian date & day"
-    (is (epsilon-diff (sut/ts->julian-date 1709128197.3056235)
-                      2460369.076))
-    (is (= (sut/julian-date->julian-day 2460369.061)
-           8825.0)))
+    (is (small-diff 2460369.076
+                    (sut/ts->julian-date timestamp)))
+    (is (= 8825.0
+           (sut/julian-date->julian-day 2460369.061))))
   (testing "Mean solar time"
-    (is (= (sut/mean-solar-time 8825.0 lon)
-           8824.903433500)))
+    (is (= 8824.903433500
+           (sut/mean-solar-time 8825.0 lon))))
   (testing "Solar mean anomaly"
-    (is (epsilon-diff (sut/mean-anomaly 8824.903433500)
-                      55.356)))
+    (is (small-diff 55.356
+                    (sut/mean-anomaly 8824.903433500))))
   (testing "Equation of the center"
-    (is (epsilon-diff (sut/equation-of-the-center (Math/toRadians 55.356))
-                      1.594)))
+    (is (small-diff 1.594
+                    (sut/equation-of-the-center (Math/toRadians 55.356)))))
   (testing "Ecliptic longitude"
-    (is (epsilon-diff (sut/ecliptic-longitude
-                       (sut/mean-anomaly 8824.903433500)
-                       (sut/equation-of-the-center
-                        (Math/toRadians 55.356)))
-                      5.932)))
+    (is (small-diff 5.932
+                    (sut/ecliptic-longitude
+                     (sut/mean-anomaly 8824.903433500)
+                     (sut/equation-of-the-center
+                      (Math/toRadians 55.356))))))
   (testing "Solar transit"
-    (is (epsilon-diff (sut/julian-date->ts
-                       (sut/solar-transit 8824.903433500
-                                          0.966
-                                          5.932))
-                      1709200418.3787212
-                      0.2            ; Double check! Epsilon too large
-                      )))
+    (is (small-diff 1709200418.3787212
+                    (sut/julian-date->ts
+                     (sut/solar-transit 8824.903433500 0.966 5.932))
+                    ;; Double check! Epsilon too large
+                    0.2)))
   (testing "Hour angle"
-    (is (epsilon-diff (sut/hour-angle
-                       lat
-                       elevation
-                       (sut/ecliptic-longitude
-                        (sut/mean-anomaly 8824.903433500)
-                        (sut/equation-of-the-center
-                         (Math/toRadians 55.356))))
-                      85.860))))
+    (is (small-diff 85.860
+                    (sut/hour-angle
+                     lat
+                     elevation
+                     (sut/ecliptic-longitude
+                      (sut/mean-anomaly 8824.903433500)
+                      (sut/equation-of-the-center
+                       (Math/toRadians 55.356)))))))
+  (testing "Sunrise & sunset"
+    (let [[sunrise sunset] (sut/sunset-sunrise-times timestamp
+                                                     lat lon elevation
+                                                     "CET")]
+      (is (= (cjt/date-time 2024 02 29 7 10 12) sunrise))
+      (is (= (cjt/date-time 2024 02 29 18 37 4) sunset)))))
